51edc1aa05d1576caf7f94b981d71f87cfb97afe
alcercu <alcercu@alumni.upv.es>
2021-12-16 18:42:16 +0100

    feat: add displays to app components preview

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ import Accordion from "./lib/accordion";
 import DropdownSelect from "./lib/dropdown/select";
 import Field from "./lib/form/field";
 import Textarea from "./lib/form/textarea";
+import DisplaySmall from "./lib/display/small";
+import Dai from "./assets/svgs/dai.svg";
+import DisplayLarge from "./lib/display/large";
+import DisplayIcon from "./lib/display/icon";
+import Balance from "./assets/svgs/balance.svg";
 
 const StyledDiv = styled.div`
   position: fixed;
@@ -77,6 +82,9 @@ const App = () => {
             message={"Error msg"}
             error
           />
+          <DisplaySmall text="250 DAI" label="Amount" icon={() => <Dai />} />
+          <DisplayLarge text="$244.08" label="ETH Price" icon={() => <Dai />} />
+          <DisplayIcon text="247" label="Disputes" icon={() => <Balance />} />
         </StyledDiv>
       </ThemeProvider>
     </React.StrictMode>
