853376e4ecb9dd82cf3cd7e581b2c19b59c71758
alcercu <alcercu@alumni.upv.es>
2022-02-02 18:18:57 +0100

    fix: export ScrollbarContainer

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -39,4 +39,6 @@ export { default as Timeline } from "../lib/progress/timeline";
 export { default as Tag } from "../lib/tag";
 export { default as Tooltip } from "../lib/tooltip";
 
+export { default as ScrollbarContainer } from "../lib/scrollbar";
+
 export { lightTheme, darkTheme } from "../styles/themes";
