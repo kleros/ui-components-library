37bd4494b5bfd2472707e58e55dcf1d3c2427702
Harman-singh-waraich <Waraichharman68@gmail.com>
2025-03-18 14:06:48 +0530

    chore: modal-react-aria

--- src/lib/container/modal.tsx
+++ src/lib/container/modal.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { cn } from "../../utils";
+import {
+  Modal as AriaModel,
+  type ModalOverlayProps,
+} from "react-aria-components";
 
-function Modal({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+function Modal({
+  className,
+  ...props
+}: ModalOverlayProps & React.RefAttributes<HTMLDivElement>) {
   return (
-    <div
+    <AriaModel
       className={cn(
         "bg-klerosUIComponentsWhiteBackground h-[200px] w-[328px]",
         "rounded-base box-border",
@@ -11,7 +18,7 @@ function Modal({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
       )}
     >
       {props.children}
-    </div>
+    </AriaModel>
   );
 }
 export default Modal;
