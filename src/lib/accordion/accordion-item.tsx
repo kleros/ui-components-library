483b499951afc79be4093138633489d3865b16c7
Harman-singh-waraich <Waraichharman68@gmail.com>
2025-06-05 12:09:43 +0530

    chore: add-ids-accordion

--- src/lib/accordion/accordion-item.tsx
+++ src/lib/accordion/accordion-item.tsx
@@ -25,19 +25,24 @@ const AccordionItem: React.FC<AccordionItemProps> = ({
   return (
     <div className="my-2">
       <Button
+        id="expand-button"
         className={cn(
           "bg-klerosUIComponentsWhiteBackground border-klerosUIComponentsStroke border",
           "hover-medium-blue hover-short-transition hover:cursor-pointer",
-          "rounded-[3px] px-8 py-[11.5px]",
+          "rounded-[3px] px-4 py-[11.5px] md:px-8",
           "flex w-full items-center justify-between",
         )}
         onPress={() => setExpanded(expanded ? -1 : index)}
       >
         {title}
         {expanded ? (
-          <Minus className={cn("fill-klerosUIComponentsPrimaryText size-4")} />
+          <Minus
+            className={cn("fill-klerosUIComponentsPrimaryText size-4 shrink-0")}
+          />
         ) : (
-          <Plus className={cn("fill-klerosUIComponentsPrimaryText size-4")} />
+          <Plus
+            className={cn("fill-klerosUIComponentsPrimaryText size-4 shrink-0")}
+          />
         )}
       </Button>
       <div
@@ -47,7 +52,7 @@ const AccordionItem: React.FC<AccordionItemProps> = ({
           "transition-[height] duration-(--klerosUIComponentsTransitionSpeed) ease-initial",
         )}
       >
-        <div className="p-8" ref={ref}>
+        <div className="p-4 md:p-8" id="body-wrapper" ref={ref}>
           {body}
         </div>
       </div>
